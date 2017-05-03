e46ad4e39cd91e3fac795f7a27b17017b778b7f4
Gregg Reynolds <reynolds-gregg@norc.org>
2017-05-03 09:58:17 -0500

    reorganize files
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- pages/example2/obs/security.clj
+++
@@ -1,6 +0,0 @@
-(ns gae.security)
-
-(def config
-  {:security [{:resource {:name "foo" :desc {:text "Foo resource security"}
-                          :url "/foo/*"}
-               :role "admin"}]})
